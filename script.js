4b6d0dff1360ff5deed31ee55582dc0b4061de8f
Franklin <frankkhuzy@gmail.com>
2023-07-28 08:02:41 -0700

    js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-script.js
+const myButton = document.querySelector('.w');
+const myDiv = document.querySelector('.third');
+
+  myButton.addEventListener('click', toggleDiv);
+
+  function toggleDiv() {
+    myDiv.classList.toggle('active')
+  }
